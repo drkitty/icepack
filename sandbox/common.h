98f2b977c5d0ed4e8fe6d8d66bd07313c808d4d5
drkitty <bumblingkitty@gmail.com>
2016-03-06 12:48:54 -0800

    Add sand

---
+++ sandbox/common.h
@@ -0,0 +1,13 @@
+#pragma once
+
+
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
+
+#define E_COMMON 1
+#define E_RARE 2
+#define E_USAGE 3
+
+
+#define print(x) fputs((x), stdout)
---
+++ sandbox/sand.c
@@ -0,0 +1,41 @@
+#include "common.h"
+
+#include <errno.h>
+#include <pwd.h>
+#include <sched.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "fail.h"
+
+
+int main(int argc, char** argv)
+{
+    if (argc < 3)
+        fatal(E_USAGE, "Usage:  sand DIR PROG [ARGS...]");
+
+    errno = 0; // man page says something about this
+    struct passwd* pw = getpwnam("sand");
+    if (pw == NULL)
+        fatal_e(E_RARE, "Can't find user \"sand\"");
+
+    if (unshare(CLONE_NEWNS) != 0)
+        fatal_e(E_COMMON, "Can't unshare mount namespace");
+
+    if (chdir(argv[1]) != 0)
+        fatal_e(E_COMMON, "Can't chdir");
+
+    if (chroot(".") != 0)
+        fatal_e(E_COMMON, "Can't chroot");
+
+    if (setgid(pw->pw_gid) != 0)
+        fatal_e(E_RARE, "Can't set gid");
+
+    if (setuid(pw->pw_uid) != 0)
+        fatal_e(E_RARE, "Can't set uid");
+
+    execvp(argv[2], argv + 2);
+
+    fatal_e(E_COMMON, "Can't execvp (check dynamic linker)");
+}
