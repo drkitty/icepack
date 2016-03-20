c1b4aef8cfa1883603cdd83bf71d5779053607d3
drkitty <bumblingkitty@gmail.com>
2016-03-19 21:40:33 -0700

    Process command-line arguments

--- icepack/common.h
+++ icepack/common.h
@@ -1,9 +1,16 @@
 #pragma once
 
 
+#define _POSIX_C_SOURCE 2
+
+
 #define E_COMMON 1
 #define E_RARE 2
+#define E_USAGE 3
 
 
 #define print(x) fputs((x), stdout)
 #define lengthof(x) (sizeof(x) / sizeof((x)[0]))
+
+
+extern int verbosity;
--- icepack/icepack.c
+++ icepack/icepack.c
@@ -1,6 +1,8 @@
 #include "common.h"
 
+#include <stdbool.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "fail.h"
 #include "lauxlib.h"
@@ -8,7 +10,53 @@
 #include "lualib.h"
 
 
-int main()
+int verbosity = 0;
+
+
+void exit_with_usage()
 {
-    fatal(E_COMMON, "Program not implemented");
+    print(
+        "Usage:  icepack [OPTIONS] FILE\n"
+        "\n"
+        "Options:\n"
+        "  -h   Show this usage message\n"
+        "  -v   Increase verbosity\n"
+    );
+}
+
+
+struct args {
+    char* name;
+} get_args(int argc, char** argv)
+{
+    struct args args;
+
+    opterr = 0;
+    while (true) {
+        int c = getopt(argc, argv, "hv");
+        if (c == -1)
+            break;
+        else if (c == '?')
+            fatal(E_USAGE, "Unrecognized option '%c'", optopt);
+        else if (c == 'h')
+            exit_with_usage();
+        else if (c == 'v')
+            ++verbosity;
+    }
+
+    if (argv[optind] == NULL)
+        fatal(E_USAGE, "Missing filename");
+    args.name = argv[optind];
+    ++optind;
+    if (argv[optind] != NULL)
+        fatal(E_USAGE, "Trailing arguments");
+
+    return args;
+}
+
+
+int main(int argc, char** argv)
+{
+    struct args args = get_args(argc, argv);
+    return 0;
 }
